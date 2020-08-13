6869458a8706f93781deace3fcf7517d1e2c70c1
Ryan Milne <ryanmilne101@gmail.com>
2020-08-13 12:41:25 -0600

    Befoe Lunch

---
+++ db/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users
+(
+    user_id SERIAL PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email VARCHAR(60),
+    password TEXT,
+    karma_score INT
+);
+CREATE TABLE posts
+(
+    post_id SERIAL PRIMARY KEY,
+    body TEXT,
+    img TEXT,
+    post_karma_score INT,
+    user_id INT REFERENCES users(user_id)
+);
+
+CREATE TABLE comments
+(
+    comment_id SERIAL PRIMARY KEY,
+    body TEXT,
+    comment_karma_score TEXT,
+    post_id INT REFERENCES post(post_id),
+    user_id INT REFERENCES users(user_id)
+);
--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
+import {Switch , Route} from 'react-router-dom'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import FrontPage from './components/FrontPage'
+import Header from './components/Header'
+
 
 class App extends React.Component {
   render(){
     return <div>
-      Good Morning!
+      <Header/>
+      <Switch>
+        <Route exact path="/" component = {Login}/>
+        <Route path="/front_page" component = {FrontPage}/>
+        <Route path="/profile" component = {Profile}/>
+      </Switch>
     </div>
   }
 }
